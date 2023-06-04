be3dc5c3ebc1bd2e3b0ee504c46c311c74f80ada
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2023-06-04 15:57:42 +0100

    [LoadApp] add pending intent

--- starter/app/src/main/java/com/udacity/NotificationUtils.kt
+++ starter/app/src/main/java/com/udacity/NotificationUtils.kt
@@ -1,12 +1,21 @@
 package com.udacity
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 
 private const val NOTIFICATION_ID = 0
 
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
     val builder = NotificationCompat.Builder(
         applicationContext,
         applicationContext.getString(R.string.load_app_notification_channel_id)
@@ -14,6 +23,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setSmallIcon(R.drawable.ic_assistant_black_24dp)
         .setContentText(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
 
     notify(NOTIFICATION_ID, builder.build())
 }
