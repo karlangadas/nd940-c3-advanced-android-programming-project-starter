9564ad0387d7e4a5aa7291c65ce127051062ebc4
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2023-06-04 19:53:22 +0100

    [LoadApp] Add initial custom button implementation

--- starter/app/src/main/java/com/udacity/ButtonState.kt
+++ starter/app/src/main/java/com/udacity/ButtonState.kt
@@ -5,4 +5,10 @@ sealed class ButtonState {
     object Clicked : ButtonState()
     object Loading : ButtonState()
     object Completed : ButtonState()
+
+    fun next() = when (this) {
+        Completed -> Clicked
+        Clicked -> Loading
+        Loading -> Completed
+    }
 }
--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,9 +1,14 @@
 package com.udacity
 
 import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import kotlin.properties.Delegates
 
@@ -12,22 +17,54 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
+    private var buttonTextResId = R.string.button_download
 
     private val valueAnimator = ValueAnimator()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { _, _, new ->
+        when (new) {
+            ButtonState.Clicked -> {
+                // trigger animation
+                buttonState = ButtonState.Loading
+            }
 
+            ButtonState.Loading -> {
+                buttonTextResId = R.string.button_loading
+                contentDescription = resources.getString(R.string.button_loading)
+            }
+
+            else -> {
+                buttonTextResId = R.string.button_download
+                contentDescription = resources.getString(R.string.button_download)
+            }
+        }
+        invalidate()
     }
 
 
     init {
-
+        buttonState = ButtonState.Completed
+        isClickable = true
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        paint.color = Color.WHITE
+        canvas.drawText(
+            resources.getString(buttonTextResId),
+            (width * 1f) / 2,
+            (height * 1f) / 2,
+            paint
+        )
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create("", Typeface.NORMAL)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -41,6 +78,12 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun performClick(): Boolean {
+        buttonState = buttonState.next()
+        return true
     }
 
 }
